7dedca20f917855c0a49f744b74ae535bf6097b1
DEMl4N <delfirmamentos@gmail.com>
2024-07-30 21:34:27 +0900

    Initial Commit

---
+++ src/main/java/com/example/crafteria_server/CrafteriaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.crafteria_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrafteriaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrafteriaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crafteria_server/CrafteriaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crafteria_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrafteriaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
