e866e31692cca3ef9bf319f17b937005631f1d4b
jhsay0314 <jhsay0314@gmail.com>
2024-08-27 05:12:15 +0900

    feat)kakaooauth2

--- src/main/java/com/example/crafteria_server/domain/user/dto/OAuth2UserInfo.java
+++ src/main/java/com/example/crafteria_server/domain/user/dto/OAuth2UserInfo.java
@@ -2,22 +2,39 @@ package com.example.crafteria_server.domain.user.dto;
 
 import com.example.crafteria_server.domain.user.entity.Role;
 import com.example.crafteria_server.domain.user.entity.User;
-import jakarta.security.auth.message.AuthException;
+import com.example.crafteria_server.global.security.AuthException;
 import lombok.Builder;
 
 import java.util.Map;
 
+import static com.example.crafteria_server.global.exception.ErrorCode.ILLEGAL_REGISTRATION_ID;
+
 @Builder
-public record OAuth2UserInfo(String name,
+public record OAuth2UserInfo(
+        String name,
         String email
         ) {
     public static OAuth2UserInfo of(String registrationId, Map<String, Object> attributes) {
-        return ofGoogle(attributes);
-        }
+        return switch (registrationId) { // registration id별로 userInfo 생성
+            case "google" -> ofGoogle(attributes);
+            case "kakao" -> ofKakao(attributes);
+            default -> throw new AuthException(ILLEGAL_REGISTRATION_ID);
+        };
+    }
+
     private static OAuth2UserInfo ofGoogle(Map<String, Object> attributes) {
         return OAuth2UserInfo.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
+                .build();
+    }
+
+    private static OAuth2UserInfo ofKakao(Map<String, Object> attributes) {
+        Map<String, Object> account = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) account.get("profile");
+
+        return OAuth2UserInfo.builder()
+                .name((String) profile.get("nickname"))
                 .build();
     }
     public User toEntity() {
