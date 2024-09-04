d600569c1a22b3d95eeaf2e8270ff7e7a2a82ba3
DEMl4N <62097643+DEMl4N@users.noreply.github.com>
2024-09-04 16:34:26 +0900

    Update CrafteriaServerApplication.java

--- src/main/java/com/example/crafteria_server/CrafteriaServerApplication.java
+++ src/main/java/com/example/crafteria_server/CrafteriaServerApplication.java
@@ -3,6 +3,7 @@ package com.example.crafteria_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Server
 
 @OpenAPIDefinition(
         servers = {
