bd1194d78ca85552bb23b3ec128478e01e06cc4e
jhsay0314 <jhsay0314@gmail.com>
2025-02-16 20:16:54 +0900

    feat)PaymentVerify

---
+++ src/main/java/com/example/crafteria_server/domain/pay/controller/PaymentController.java
@@ -0,0 +1,31 @@
+package com.example.crafteria_server.domain.pay.controller;
+
+import com.example.crafteria_server.domain.pay.dto.PaymentDto;
+import com.example.crafteria_server.domain.pay.service.PaymentService;
+import com.example.crafteria_server.global.response.JsonBody;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j(topic = "PaymentController")
+@RequestMapping("/api/v1/payment")
+public class PaymentController {
+
+    private PaymentService paymentService;
+
+    @PostMapping("/complete")
+    public JsonBody<PaymentDto.PaymentResultDto> completePayment(@RequestBody PaymentDto.PaymentRequestDto paymentRequest) {
+        try {
+            PaymentDto.PaymentResultDto paymentResult = paymentService.processPayment(paymentRequest.getPaymentId(), paymentRequest.getOrder());
+            return JsonBody.of(200, "성공", paymentResult);
+        } catch (Exception e) {
+            return JsonBody.of(400, "실패: " + e.getMessage(), null);
+        }
+    }
+}
---
+++ src/main/java/com/example/crafteria_server/domain/pay/dto/PaymentDto.java
@@ -0,0 +1,38 @@
+package com.example.crafteria_server.domain.pay.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+public class PaymentDto {
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PaymentRequestDto {
+        private String paymentId;
+        private Long order;
+
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PaymentResultDto {
+        private String status;
+        private String message;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class PaymentResponse {
+        private String status;
+        private BigDecimal amount;
+    }
+}
