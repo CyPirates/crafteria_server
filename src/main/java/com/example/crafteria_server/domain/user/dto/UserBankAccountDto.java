1e57dd1307c49a99536880aa0de9dbe728aaf310
jhsay0314 <jhsay0314@gmail.com>
2025-10-19 17:17:30 +0900

    fix)계좌종류 추가

--- src/main/java/com/example/crafteria_server/domain/user/dto/UserBankAccountDto.java
+++ src/main/java/com/example/crafteria_server/domain/user/dto/UserBankAccountDto.java
@@ -13,4 +13,9 @@ public class UserBankAccountDto {
     @Schema(description = "계좌번호(공백/하이픈 허용, 서버에서 정규화)", example = "110-123-456789")
     @NotBlank
     private String accountNumber;
+
+    @Schema(description = "계좌 종류 (은행명 등)", example = "국민은행")
+    private String accountType;
+
 }
+
